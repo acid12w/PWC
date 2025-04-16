de2d7a1afa0e56a3f74c6c44b72678946de35da3
adrian <adri202.aa@gmail.com>
2025-04-16 00:37:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const certification = document.querySelector('#Certification-type');
+const courseType = document.querySelectorAll("#course-type");
+
+
+certification.addEventListener("input", function(e){
+    let options = e.target.selectedOptions;
+    var values = Array.from(options).map(({ value }) => value);
+    
+ 
+    courseType.forEach(course => {
+       
+        if(course.dataset.courseType === values[0]) {
+            // console.log(course);
+            course.classList.remove('hidden');
+            course.classList.add('toggleShow');
+        }
+    })
+
+
+})
