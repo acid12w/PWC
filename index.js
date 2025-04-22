51a8fc5e295def870e117f11938d2d3956f5a2b6
adrian <adri202.aa@gmail.com>
2025-04-21 22:53:10 -0500

    course pages build out

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ certification.addEventListener("input", function(e){
     
  
     courseType.forEach(course => {
-       
         if(course.dataset.courseType === values[0]) {
             // console.log(course);
             course.classList.remove('hidden');
             course.classList.add('toggleShow');
+        }else {
+             console.log(course); 
+             course.classList.add('hidden');
+             course.classList.remove('toggleShow');
         }
     })
-
-
 })
