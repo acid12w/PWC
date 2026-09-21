9d0d7488042bc2afdd6a1ca4f8927e854ccfdbd9
adrian <adri202.aa@gmail.com>
2026-09-21 09:52:47 -0500

    new hew image Ani + PWC Lanuch

--- index.js
+++ index.js
@@ -1,66 +1,61 @@
 // MOBILE NAV
 
-const mobileNav = document.querySelector('#mobile-nav');
-const navbarDropdown = document.querySelector('#navbar-dropdown');
+const mobileNav = document.querySelector("#mobile-nav");
+const navbarDropdown = document.querySelector("#navbar-dropdown");
 
-mobileNav.addEventListener('click', function(e){
-    navbarDropdown.classList.toggle('hidden');
+mobileNav.addEventListener("click", function (e) {
+  navbarDropdown.classList.toggle("hidden");
 });
 
 gsap.to(".slide-in-up", {
-    scrollTrigger:{ 
-        Trigger: ".about-holder", // start animation when ".box" enters the viewport
-        start: "top center",
-        scrub: 1, 
-        toggleAction: 'restart pause reverse pause'
-    },
-    y: 0,
-    duration: 1.5,
-    ease: "power1.out",
-  });
-
+  scrollTrigger: {
+    Trigger: ".about-holder", // start animation when ".box" enters the viewport
+    start: "top center",
+    scrub: 1,
+    toggleAction: "restart pause reverse pause",
+  },
+  y: 0,
+  duration: 1.5,
+  ease: "power1.out",
+});
 
 // NAV DROP DOWN MENU
-const dropdownBtn = document.querySelector('#dropdown-toggle');
-const dropdownMenu = document.querySelector('#dropdownNavbar');
+const dropdownBtn = document.querySelector("#dropdown-toggle");
+const dropdownMenu = document.querySelector("#dropdownNavbar");
 
-
-dropdownBtn.addEventListener('mouseover', function(e){
-    console.log(e.target)
-    dropdownMenu.classList.remove('hidden');
+dropdownBtn.addEventListener("mouseover", function (e) {
+  console.log(e.target);
+  dropdownMenu.classList.remove("hidden");
 });
 
-dropdownBtn.addEventListener('mouseleave', function(){
-    console.log("leave")
-    dropdownMenu.classList.add('hidden');
+dropdownBtn.addEventListener("mouseleave", function () {
+  console.log("leave");
+  dropdownMenu.classList.add("hidden");
 });
 
-dropdownMenu.addEventListener('mouseover', function(){
-    dropdownMenu.classList.remove('hidden');
+dropdownMenu.addEventListener("mouseover", function () {
+  dropdownMenu.classList.remove("hidden");
 });
 
-dropdownMenu.addEventListener('mouseleave', function(){
-    dropdownMenu.classList.add('hidden');
+dropdownMenu.addEventListener("mouseleave", function () {
+  dropdownMenu.classList.add("hidden");
 });
-
 
 //APPLICATION FORM
 // const certification = document.querySelector('#Certification-type');
 // const courseType = document.querySelectorAll("#course-type");
 
-
 // certification.addEventListener("input", function(e){
 //     let options = e.target.selectedOptions;
 //     var values = Array.from(options).map(({ value }) => value);
-    
- 
+
 //     courseType.forEach(course => {
 //         if(course.dataset.courseType === values[0]) {
 //             // console.log(course);
 //             course.classList.remove('hidden');
 //             course.classList.add('toggleShow');
 //         }else {
-//              console.log(course); 
+//              console.log(course);
 //              course.classList.add('hidden');
 //              course.classList.remove('toggleShow');
 //         }
@@ -72,73 +67,70 @@ const cards = document.querySelectorAll(".card");
 const buttonHolder = document.querySelector(".card-btn__holder");
 const btnDep = document.querySelectorAll("#btn-dep");
 
+const tabFunc = (buttonGroup, el, buttons) => {
+  if (buttonGroup === null) return;
 
-const tabFunc = (buttonGroup, el, buttons)=> {
-
-if (buttonGroup === null) return;
-
-buttonGroup.addEventListener("click", function (e) {
+  buttonGroup.addEventListener("click", function (e) {
     const clicked = e.target.dataset.type;
-  
+
     if (!clicked) return;
-      
-      el.forEach((el) =>{
-          {if(clicked !== el) {el.classList.add("hidden")}}}
-      );
-  
-      buttons.forEach(el => {
-          el.classList.remove("text-[#F99F02]");
-         
-          if(el === e.target){
-              e.target.classList.remove("text-gray-700");
-              e.target.classList.add("text-[#F99F02]");
-          } 
-      })
-    
-  
-      document
-          .querySelectorAll(`.card__type--${clicked}`)
-          .forEach((el) => el.classList.remove("hidden"));
+
+    el.forEach((el) => {
+      {
+        if (clicked !== el) {
+          el.classList.add("hidden");
+        }
+      }
+    });
+
+    buttons.forEach((el) => {
+      el.classList.remove("text-[#F99F02]");
+
+      if (el === e.target) {
+        e.target.classList.remove("text-gray-700");
+        e.target.classList.add("text-[#F99F02]");
+      }
+    });
+
+    document
+      .querySelectorAll(`.card__type--${clicked}`)
+      .forEach((el) => el.classList.remove("hidden"));
   });
-}
+};
 
+tabFunc(buttonHolder, cards, btnDep);
 
-  tabFunc(buttonHolder, cards, btnDep);
-  
-  
-  
-  window.onscroll = function() {scrollFunction()};
-  
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      // document.getElementById("navbar").style.padding = "10px 0px 0px 0px";
-      document.getElementById("logo").style.height = "4em";
-    } else {
-      // document.getElementById("navbar").style.padding = "10px 0px 0px 0px";
-      document.getElementById("logo").style.height = "6em";
-    }
-  }
+window.onscroll = function () {
+  scrollFunction();
+};
 
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     // document.getElementById("navbar").style.padding = "10px 0px 0px 0px";
+//     document.getElementById("logo").style.height = "4em";
+//   } else {
+//     // document.getElementById("navbar").style.padding = "10px 0px 0px 0px";
+//     document.getElementById("logo").style.height = "6em";
+//   }
+// }
 
-  const slide = gsap.utils.toArray("#infinte-slide");
+const slide = gsap.utils.toArray("#infinte-slide");
 
-  gsap.set(slide, {
-    x: -1350,
-  });
+gsap.set(slide, {
+  x: -1350,
+});
 
-  gsap.to(slide, {
-    x: 1500,
-    duration: 30,
-    ease: "none",
-    repeat: -1,
-    repeatWithPause: 0.1,
-  });
+gsap.to(slide, {
+  x: 1500,
+  duration: 30,
+  ease: "none",
+  repeat: -1,
+  repeatWithPause: 0.1,
+});
 
+const closeAlert = document.querySelector("#close-alert");
+const alert = document.querySelector("#alert");
 
-  const closeAlert = document.querySelector("#close-alert");
-  const alert = document.querySelector("#alert");
-
-  closeAlert.addEventListener("click", function(){
-    alert.style.display = "none";
-  })
+closeAlert.addEventListener("click", function () {
+  alert.style.display = "none";
+});
